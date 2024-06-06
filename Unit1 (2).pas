584e82e8c74786795197e2537408e995cc7134c0
audeliadwiidfryani0010 <audeliadwi@gmail.com>
2024-06-06 15:29:09 +0700

    Add files via upload

---
+++ Unit1 (2).pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Menus;
+
+type
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    FILE1: TMenuItem;
+    DATAMASTER1: TMenuItem;
+    RANSAKSI1: TMenuItem;
+    LAPORAN1: TMenuItem;
+    LOGIN1: TMenuItem;
+    LOGOUT1: TMenuItem;
+    KATEGORI1: TMenuItem;
+    SATUAN1: TMenuItem;
+    SUPPLIER1: TMenuItem;
+    KOSTUMER1: TMenuItem;
+    PENJUALAN1: TMenuItem;
+    PEMBELIAN1: TMenuItem;
+    LAPORANPENJUALAN1: TMenuItem;
+    LAPORANSTOKBARANG1: TMenuItem;
+    LAPORANBARANG1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
